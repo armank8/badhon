04ea8f6aa50794717143c04a86289ff05c2a85d0
Arman-Kabir <armankabir8@gmail.com>
2024-05-23 19:51:52 +0600

    navbar

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -2,7 +2,8 @@ import a from "../../assets/img/a.jpg";
 import b from "../../assets/img/b.jpeg";
 export default function Banner() {
   return (
-    <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-3 text-white my-10">
+    <div className="bg-[#F8F8F6]">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-3 text-white py-10 ">
       <div>
         <img src={a} alt="" />
         <button className="btn btn-primary mt-12 text-white px-10">Visit Now</button>
@@ -11,6 +12,7 @@ export default function Banner() {
         <img src={b} alt="" />
         <button className="btn btn-primary mt-12 text-white px-10">Read More</button>
       </div>
+    </div>
     </div>
   );
 }
--- src/pages/Shared/TopBar.jsx
+++ src/pages/Shared/TopBar.jsx
@@ -2,7 +2,7 @@ export default function TopBar() {
   return (
     <div className="bg-black flex justify-between text-white px-20 h-9 items-center">
       <div>Login/Register</div>
-      <p className="text-red-700">See 40 Years Journey of ActionAid Bangladesh</p>
+      <p className="text-red-500 font-bold">See 20 Years Journey of Badhon Bagerhat,Bangladesh</p>
       <div className="flex">
         <p>Follow Us On: </p>
         <div>social</div>

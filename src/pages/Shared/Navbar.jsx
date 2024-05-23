3bffc3c6b4f79c050e207df527762d0173dffe98
Arman-Kabir <armankabir8@gmail.com>
2024-05-23 20:03:08 +0600

    megamenu

--- src/pages/Shared/Navbar.jsx
+++ src/pages/Shared/Navbar.jsx
@@ -1,69 +1,111 @@
-import { Link } from "react-router-dom";
-
 export default function Navbar() {
   return (
-    <div className=" bg-base-100 h-20">
-      <div className="navbar max-w-7xl mx-auto">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />
-              </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+    <div className="navbar bg-base-100">
+        {/* title and other */}
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <Link
-            to="/"
-            className="btn btn-ghost text-3xl text-primary font-bold"
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            Badhon
-          </Link>
-        </div>
-        <div className="dropdown navbar-center hidden lg:flex">
-          <ul className=" menu menu-horizontal px-1 ">
             <li>
-              <details>
-                <summary>Parent</summary>
-              </details>
+              <a>Item 1</a>
+            </li>
+            <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a>Item 3</a>
             </li>
           </ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn btn-primary px-12">Donate</a>
-        </div>
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      {/* middle menu */}
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          
+          <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="menu xl:menu-horizontal lg:min-w-max bg-base-200 rounded-box">
+  <li>
+    <a>Solutions</a>
+    <ul>
+      <li><a>Design</a></li>
+      <li><a>Development</a></li>
+      <li><a>Hosting</a></li>
+      <li><a>Domain register</a></li>
+    </ul>
+  </li>
+  <li>
+    <a>Enterprise</a>
+    <ul>
+      <li><a>CRM software</a></li>
+      <li><a>Marketing management</a></li>
+      <li><a>Security</a></li>
+      <li><a>Consulting</a></li>
+    </ul>
+  </li>
+  <li>
+    <a>Products</a>
+    <ul>
+      <li><a>UI Kit</a></li>
+      <li><a>Wordpress themes</a></li>
+      <li><a>Wordpress plugins</a></li>
+      <li>
+        <a>Open source</a>
+        <ul>
+          <li><a>Auth management system</a></li>
+          <li><a>VScode theme</a></li>
+          <li><a>Color picker app</a></li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+  <li>
+    <a>Company</a>
+    <ul>
+      <li><a>About us</a></li>
+      <li><a>Contact us</a></li>
+      <li><a>Privacy policy</a></li>
+      <li><a>Press kit</a></li>
+    </ul>
+  </li>
+</ul>
+            </details>
+          </li>
+          
+        </ul>
+      </div>
+
+      {/* end button */}
+      <div className="navbar-end">
+        <a className="btn">Button</a>
       </div>
     </div>
   );

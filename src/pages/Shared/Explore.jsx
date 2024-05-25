792e0097f635995200c7550c626d912e1b1d2c2a
Arman-Kabir <armankabir8@gmail.com>
2024-05-25 09:28:02 +0600

    dynamic tabs created

--- src/pages/Shared/Explore.jsx
+++ src/pages/Shared/Explore.jsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Explore() {
-    // const [active,setActive] = 
+  const [activeButton, setActiveButton] = useState("all");
+
+  const handleButtonClick = (buttonName) => {
+    console.log(activeButton);
+    setActiveButton(buttonName);
+  };
   return (
     <div className="max-w-7xl mx-auto my-28">
       {/* titloe */}
@@ -13,11 +19,31 @@ export default function Explore() {
 
       <div className="">
         <div className="blogs_menus flex justify-start gap-6 border-b-2">
-            <button className="px-6 py-2  hover:border-b-2 border-primary active:border-primary">All</button>
-            <button className="px-6 py-2  hover:border-b-2 border-primary active:border-primary">News</button>
-            <button className="px-6 py-2  hover:border-b-2 border-primary active:border-primary">Blogs</button>
+          <button
+            className={`px-6 py-2  hover:border-b-2 border-primary active:border-primary ${
+              activeButton === "all" ? "border-b-2" : ""
+            }`}
+            onClick={() => handleButtonClick("all")}
+          >
+            All
+          </button>
+          <button
+            className={`px-6 py-2  hover:border-b-2 border-primary active:border-primary ${
+              activeButton === "news" ? "border-b-2" : ""
+            }`}
+            onClick={() => handleButtonClick("news")}
+          >
+            News
+          </button>
+          <button
+            className={`px-6 py-2  hover:border-b-2 border-primary active:border-primary ${
+              activeButton === "blogs" ? "border-b-2" : ""
+            }`}
+            onClick={() => handleButtonClick("blogs")}
+          >
+            Blogs
+          </button>
         </div>
-
       </div>
     </div>
   );

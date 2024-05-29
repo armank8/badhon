5a66f7dd419c556f927a0a67abe1c8daea0ea93e
Arman-Kabir <armankabir8@gmail.com>
2024-05-29 23:05:19 +0600

    banner image jsx done and package updated

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -3,16 +3,28 @@ import b from "../../assets/img/b.jpeg";
 export default function Banner() {
   return (
     <div className="bg-[#F8F8F6]">
-        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-3 text-white py-10 ">
-      <div>
-        <img src={a} alt="" />
-        <button className="btn btn-primary mt-12 text-white px-10">Visit Now</button>
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-3 text-white py-10 ">
+        <div>
+          {/* <img src={a} alt="" /> */}
+          <div className="w-full bg-primary h-[437px] flex justify-center items-center">
+            <div className="flex flex-col">
+              <span className="text-7xl font-black">20</span>
+              <span>Years of</span>
+              <span className="text-3xl font-black">Badhon NGO</span>
+              <span className="text-xl">Bagerhat,Bangladesh.</span>
+            </div>
+          </div>
+          <button className="btn btn-primary mt-12 text-white px-10">
+            Visit Now
+          </button>
+        </div>
+        <div>
+          <img src={b} alt="" />
+          <button className="btn btn-primary mt-12 text-white px-10">
+            Read More
+          </button>
+        </div>
       </div>
-      <div>
-        <img src={b} alt="" />
-        <button className="btn btn-primary mt-12 text-white px-10">Read More</button>
-      </div>
-    </div>
     </div>
   );
 }
